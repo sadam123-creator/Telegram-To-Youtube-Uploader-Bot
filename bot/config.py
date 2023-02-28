8d3af95788d45f4bb22bdd33f60362d8a165c8b4
sadam123-creator <73885372+sadam123-creator@users.noreply.github.com>
2023-02-28 20:31:37 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -2,25 +2,130 @@ import os
 
 class Config:
 
-    BOT_TOKEN = ""
+    BOT_TOKEN = "5857558931:AAGnJgKu7IZgfQjZUWpk33u-U5IgJLdu5Rc"
 
-    SESSION_NAME = ""
+    SESSION_NAME = "@Postedsada_bot"
 
-    API_ID = ""
+    API_ID = "26825478"
 
-    API_HASH = ""
+    API_HASH = "2aa11a71815324151f2468c75c446ad6"
 
-    CLIENT_ID = ""
+    CLIENT_ID = "921365874485-gnei68nrd8kul5ug8u7kbhed7f7762gs.apps.googleusercontent.com"
 
-    CLIENT_SECRET = ""
+    CLIENT_SECRET = "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"
 
-    AUTH_USERS = [942731625]
+    AUTH_USERS = [810645599]
 
-    VIDEO_DESCRIPTION = ""
+    VIDEO_DESCRIPTION = "
+Please like and subscribe for more 
 
-    VIDEO_CATEGORY = ""
 
-    VIDEO_TITLE_PREFIX = ""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#unusual videos #unusual videos compilation #weird videos #weird videos compilation #meme compilation #dank meme compilation #unusual meme compilation #weird meme compilation #creepy meme compilation #memes compilation #dank memes #funny #dank memes vine compilation #try not to laugh #fresh memes #emisoccer #clumsy #hefty #comment awards #grandayy #tiktok #tik tok memes #rewind #airpods #big chungus #pewdiepie vs tseries #afv #afv 2023 #fails #fail #funniest fails #fails of the week #americas funniest videos #funny videos #fail videos #afv funny videos #afv funniest videos #fail compilation #epic fails #try not to laugh #funny fails #instant regret #instant karma #fail 2022 #tiktok fail #fails compilation 2023 #funny fails 2023 #fails 2023 #Outdoor fails #Try not to laugh challenge #funny videos compilation #funny videos compilation 2023 #afv 2022 #Try not to laugh #funny video #tiktok #memes #tiktok memes #funny #funny memes #tiktok funny memes #funny videos #try not to laugh #try not to laugh challenge #tiktok videos #bad day #laugh challenge #funny moments #this wil make you laugh #lmao #if you laugh you lose #try not to laugh impossible #top things #dank #watch people die inside #dank memes #you laugh you lose #instant karma #what could go wrong #instant regret #Funny moments #Funny fails #Fails #epic fails #pranks #bad ideas #comedy #funny video,failarmy #fails #fail army funny #failarmy 2023 #fails of the week #fails 2023 #fails compilation #fail compilation 2022 #funny fails 2023 #epic fails #epic fails 2023 #funny videos 2023 #funny videos #try not to laugh memes #try not to laugh #fails of the week failarmy #fail of the week 2023 #car fails #expensive fails #worst fails #truck fails #skateboarding #best fails 2023 #cctv fails #cctv footage #bad day #razy clips #instant regret #meme #dank meme,new funny video #comedy videos #funny clips video #funny video 2021,very funny videos #funny videos #comedy videos #funny videos 2021 #comedy videos 2021 #new funny videos 2021 #funny day #funny fails compilation #funnty videos 2020 #funny videos #funny video 2020 #funny fails video #funny vines #try not to laugh impossible #funny fails #try not to laugh hard #try not to laugh clean #life awesome #try not to laugh challenge #try not to laugh #fail video #clean vines #clean fails #fails #funny videos 2023 #funny videos #funny fails 2023 #funny video #funny videos compilation 2023 #funny 2023 #afv funny videos #funny fail video #funny videos compilation #funniest video #afv funniest videos #fails 2023 #afv videos #best fails 2023 #fail videos #funny fails #americas funniest videos #afv 2023 #funny afv #americas funniest home videos #afv 2022 #vine best fun 2023 #funniest fails #fail 2022 #vines #funny #fails compilation 2023 #outdoor fails #funny videos #funny video #afv funny videos #funny fail video #funniest video #afv funniest videos #afv videos #funny videos compilation #fail videos #funny fails #funny afv #americas funniest videos #funny videos compilation 2023 #funny fails 2023 #americas funniest home videos #funniest fails #afv episodes #outdoor fails #instant regret #best fails #epic fails #instant karma #fails #best fails 2023 #try not to laugh #fail comp #tiktok fail
+"
+
+    VIDEO_CATEGORY = "Entertainment"
+
+    VIDEO_TITLE_PREFIX = "Try Not To Laugh Best Funny 
+Videos Compilation Part "
 
     VIDEO_TITLE_SUFFIX = ""
     
